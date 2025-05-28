15bc2ef5280b1b72271caf5d3f7f67adba3072d0
Amara Tie <amara.tie@stu.ocsb.ca>
2025-05-28 17:41:21 +0000

    hello_world-01.py

---
+++ hello_world-01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created by: Amara Tie 
+# Date: May 28, 2025
+# This program displays our opening message
+
+def game_scene():
+    print("\n")
+    print("Welcome to Cupid's \n ")
+    print("Matchmaking game!")
+
+    while True:
+        pass
+
+if __name__== "__main__":
+    game_scene()
