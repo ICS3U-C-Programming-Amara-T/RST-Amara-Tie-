872a22ddb92c82a3d3804968a2563c1fae1f7cf4
Amara Tie <amara.tie@stu.ocsb.ca>
2025-05-28 17:55:42 +0000

    hello_world-01.py

--- hello_world-01.py
+++ hello_world-01.py
@@ -3,13 +3,13 @@
 # Date: May 28, 2025
 # This program displays our opening message
 
+# Opening message function 
 def game_scene():
     print("\n")
     print("Welcome to Cupid's \n ")
     print("Matchmaking game!")
 
-    while True:
-        pass
+
 
 if __name__== "__main__":
     game_scene()
