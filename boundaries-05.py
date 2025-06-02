4e19d91a54877433d182449ec239d16e3c50ba09
Amara Tie <amara.tie@stu.ocsb.ca>
2025-06-02 14:35:23 +0000

    boundaries-05.py

--- boundaries-05.py
+++ boundaries-05.py
@@ -4,41 +4,52 @@
 # This program displays our opening message
 
 import ugame
-import stage 
+import stage
+import constants
 
 def game_scene():
 
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_X)
 
-    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constnats.SPRITE_SIZE))
+    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
 
-    game = stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, constants.FPS)
 
     game.layers = [ship] + [background]
 
     game.render_block()
 
     while True: 
+        
         keys = ugame.buttons.get_pressed()
+        
         if keys & ugame.K_X:
-            print("A")
+            pass
         if keys & ugame.K_O:
-            print("B")
+            pass
         if keys & ugame.K_START:
-            print("Start")
+            pass
         if keys & ugame.K_SELECT:
-            print("Select")
+            pass
         if keys & ugame.K_RIGHT:
-            ship.move(ship.x + 1, ship.y)
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
+            else:
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
         if keys & ugame.K_LEFT:
-            ship.move(ship.x - 1, ship.y)
+            if ship.x >= 0:
+                ship.move(ship.x - 1, ship.y)
+            else:
+                ship.move(0, ship.y)
         if keys & ugame.K_UP:
-            ship.move(ship.x, ship.y - 1)
-        if keys & ugame.K_RIGHT:
-            ship.move(ship.x, ship.y + 1)
+            pass
+        if keys & ugame.K_DOWN:
+            pass
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__== "__main__":
--- buttons-04.py
+++ buttons-04.py
@@ -7,9 +7,25 @@ import ugame
 import stage 
 
 def game_scene():
+    # Background image 
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    # Sets background size (10 x 8 and 16 x 16)
+    background = stage.Grid(image_bank_background, 10, 8)
 
-    while True: 
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # Set the frame to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # Set the images that will show up
+    game.layers = [ship] + [background]
+    # Render background
+    game.render_block()
+
+    # Game Loop
+    while True:
+        # control keys
         keys = ugame.buttons.get_pressed()
+        
         if keys & ugame.K_X:
             print("A")
         if keys & ugame.K_O:
@@ -26,6 +42,10 @@ def game_scene():
             ship.move(ship.x, ship.y - 1)
         if keys & ugame.K_RIGHT:
             ship.move(ship.x, ship.y + 1)
+        # Redraw Sprite
+        game.render_sprites([ship])
+        game.tick()
+        
         
 
 if __name__== "__main__":
