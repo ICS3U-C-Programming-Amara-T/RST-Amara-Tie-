ebb5136eaf687b4afc04943da65d20d7ea02d068
Amara Tie <amara.tie@stu.ocsb.ca>
2025-06-04 16:05:39 +0000

    boundaries-05.py

--- boundaries-05.py
+++ boundaries-05.py
@@ -8,22 +8,25 @@ import stage
 import constants
 
 def game_scene():
-
+    # Background image
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
-
+    # Sets background size (10 x 8 and 16 x 16)
     background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_X)
 
     ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
-
+    # Set the frame to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
 
+    # Set the images that will show up
     game.layers = [ship] + [background]
 
+    # Render background
     game.render_block()
 
+    # Game Loop
     while True: 
-        
+        # control keys with boundaries
         keys = ugame.buttons.get_pressed()
         
         if keys & ugame.K_X:
@@ -48,6 +51,8 @@ def game_scene():
             pass
         if keys & ugame.K_DOWN:
             pass
+
+        # Redraw Sprite
         game.render_sprites([ship])
         game.tick()
 
